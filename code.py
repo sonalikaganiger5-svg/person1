b0b5815e9faa02c6307103aba063eecc2ee0300b
sonalikaganiger5-svg <sonalikaganiger5@gmail.com>
2026-09-08 10:34:48 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
-print("first_name:sonalika")
-print("last_name:ganiger")
-print("collage:kle.tech")
-print("division:A")
+first_name = "Sonalika"
+last_name = "Ganiger"
+division = "A"
+college = "KLE Technological University"
 
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Division:", division)
+print("College:", college)
